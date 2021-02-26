5c6dd1dcceb7a9c4b8e3c7189c9b00d0efb4f647
Ricardo Flores <rik.fm1809@gmail.com>
2021-02-26 18:20:26 -0500

    add react-router-dom, Bootstrap CDN and config eslint

---
+++ src/CalendarApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <h1>calendar app</h1> <button className="btn btn-primary">hola</button>
+        </div>
+    )
+}
