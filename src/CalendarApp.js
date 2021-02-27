0ac273970a7e2ae6e11fb1e3f03d539e8b7e9b2b
Ricardo Flores <rik.fm1809@gmail.com>
2021-02-26 20:48:31 -0500

    add component LoginScreen, CalendarScreen and Routing

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { AppRouter } from './routers/AppRouter'
+
 export const CalendarApp = () => {
     return (
         <div>
-            <h1>calendar app</h1> <button className="btn btn-primary">hola</button>
+            <AppRouter  />
         </div>
     )
 }
